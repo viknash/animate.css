112bec24e8408ba73b2291fc5f95967db9509132
Viknash <viknash@hobbiz.com>
2015-05-17 23:48:43 +0800

    Update cssmin to generate source maps

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,84 +1,87 @@
 module.exports = function (grunt) {
 
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    var concatAnim;
+  var concatAnim;
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            dist: {
-                src: ['source/_base.css', 'source/**/*.css'], // _base.css required for .animated helper class
-                dest: 'animate.css'
-            }
-        },
+    concat: {
+      dist: {
+        src: ['source/_base.css', 'source/**/*.css'], // _base.css required for .animated helper class
+        dest: 'animate.css'
+      }
+    },
 
-        autoprefixer: { // https://github.com/nDmitry/grunt-autoprefixer
-            options: {
-                browsers: ['last 2 versions', 'bb 10']
-            },
-            no_dest: {
-                src: 'animate.css' // output file
-            }
-        },
+    autoprefixer: { // https://github.com/nDmitry/grunt-autoprefixer
+      options: {
+        browsers: ['last 2 versions', 'bb 10']
+      },
+      no_dest: {
+        src: 'animate.css' // output file
+      }
+    },
 
-        cssmin: {
-            minify: {
-                src: ['animate.css'],
-                dest: 'animate.min.css',
-            }
-        },
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      minify: {
+        src: ['animate.css'],
+        dest: 'animate.min.css',
+      }
+    },
 
-        watch: {
-            css: {
-                files: ['source/**/*', 'animate-config.json'],
-                tasks: ['default']
-            }
+    watch: {
+      css: {
+        files: ['source/**/*', 'animate-config.json'],
+        tasks: ['default']
+      }
+    }
+
+  });
+
+  // fuction to perform custom task
+  concatAnim = function () {
+
+    var categories = grunt.file.readJSON('animate-config.json'),
+      category, files, file,
+      target = ['source/_base.css'],
+      count = 0;
+
+    for (category in categories) {
+      if (categories.hasOwnProperty(category)) {
+        files = categories[category];
+        for (file in files) {
+          if (files.hasOwnProperty(file) && files[file]) {
+            target.push('source/' + category + '/' + file + '.css');
+            count += 1;
+          }
         }
+      }
+    }
 
+    if (!count) {
+      grunt.log.writeln('No animations activated.');
+    } else {
+      grunt.log.writeln(count + (count > 1 ? ' animations' : ' animation') + ' activated.');
+    }
+
+    grunt.config('concat', {
+      'animate.css': target
     });
+    grunt.task.run('concat');
 
-    // fuction to perform custom task
-    concatAnim = function () {
+  };
 
-        var categories = grunt.file.readJSON('animate-config.json'),
-            category, files, file,
-            target = ['source/_base.css'],
-            count = 0;
-
-        for (category in categories) {
-            if (categories.hasOwnProperty(category)) {
-                files = categories[category]
-                for (file in files) {
-                    if (files.hasOwnProperty(file) && files[file]) {
-                        target.push('source/' + category + '/' + file + '.css');
-                        count += 1;
-                    }
-                }
-            }
-        }
-
-        if (!count) {
-            grunt.log.writeln('No animations activated.');
-        } else {
-            grunt.log.writeln(count + (count > 1 ? ' animations' : ' animation') + ' activated.');
-        }
-
-        grunt.config('concat', {
-            'animate.css': target
-        });
-        grunt.task.run('concat');
-
-    };
-
-    grunt.loadNpmTasks('grunt-travis-lint');
-    // register task
-    grunt.registerTask('concat-anim', 'Concatenates activated animations', concatAnim); // custom task
-    grunt.registerTask('all', ['concat-anim', 'autoprefixer', 'cssmin']);
-    grunt.registerTask('default', ['all']);
-    grunt.registerTask('travis', ['travis-lint', 'all']);
-    grunt.registerTask('dev', ['watch']);
+  grunt.loadNpmTasks('grunt-travis-lint');
+  // register task
+  grunt.registerTask('concat-anim', 'Concatenates activated animations', concatAnim); // custom task
+  grunt.registerTask('all', ['concat-anim', 'autoprefixer', 'cssmin']);
+  grunt.registerTask('default', ['all']);
+  grunt.registerTask('travis', ['travis-lint', 'all']);
+  grunt.registerTask('dev', ['watch']);
 
 };
